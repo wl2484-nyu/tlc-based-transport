d6ac7e140803657f0ff24fe13f06e04cd998e5b1
Wan-Yu Lin <wl2484@nyu.edu>
2023-11-27 20:01:42 -0500

    Update CleanTaxiZoneLookup code

--- code/src/main/scala/etl/CleanTaxiZoneLookup.scala
+++ code/src/main/scala/etl/CleanTaxiZoneLookup.scala
@@ -1,25 +1,46 @@
 package etl
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object CleanTaxiZoneLookup {
-  val input_path = "project/data/source/tlc/zone_lookup"
-  val output_path = "project/data/clean/tlc/zone_lookup"
+  val inputPath = "project/data/source/tlc/zone_lookup"
+  val outputPath = "project/data/clean/tlc/zone_lookup"
 
-  def clean(df: DataFrame): DataFrame = {
-    // TODO: clean df, extract and rename selected columns
-    df
+  case class TaxiZoneLookupTable(location_id: Int, borough: String, zone: String)
+
+  def clean(spark: SparkSession, df: DataFrame): Dataset[TaxiZoneLookupTable] = {
+    import spark.implicits._
+
+    df.drop("service_zone")
+      .withColumnRenamed("LocationID", "location_id")
+      .withColumnRenamed("Borough", "borough")
+      .withColumnRenamed("Zone", "zone")
+      .as[TaxiZoneLookupTable]
+      .filter(r => r.borough != "Unknown")
   }
 
-  def save(df: DataFrame, path: String): Unit = {
-    // TODO: save
+  def save(ds: Dataset[TaxiZoneLookupTable], path: String): Unit = {
+    val sorted_ds = ds.repartition(col("borough"))
+      .sortWithinPartitions("location_id")
+
+    sorted_ds.select("borough")
+      .distinct
+      .collect
+      .map(_(0).asInstanceOf[String])
+      .foreach(b =>
+        sorted_ds.filter(r => r.borough == b)
+          .write
+          .option("header", true)
+          .csv(f"$outputPath/$b")
+      )
   }
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("CleanTaxiZoneLookup").getOrCreate()
 
-    val raw_df = spark.read.option("header", true).option("inferSchema", true).csv(input_path)
-    val clean_df = clean(raw_df)
-    save(clean_df, output_path)
+    val raw_df = spark.read.option("header", true).option("inferSchema", true).csv(inputPath)
+    val clean_df = clean(spark, raw_df)
+    save(clean_df, outputPath)
   }
 }
