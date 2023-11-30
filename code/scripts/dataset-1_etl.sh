525b8d3ddb93c46c889fd4416ff87c4ddff39718
Wan-Yu Lin <wl2484@nyu.edu>
2023-11-30 00:30:42 -0500

    Update dataset-*_etl.sh

--- code/scripts/dataset-1_etl.sh
+++ code/scripts/dataset-1_etl.sh
@@ -9,9 +9,9 @@ APP_VER="0.3.0"
 SCALA_VER="2.12"
 CLASS_NAME="etl.TaxiZones"
 JAR_PATH="tlc-based-transport_${SCALA_VER}-${APP_VER}.jar"
-SOURCE_PATH="/user/wl2484_nyu_edu/project/data/source/tlc/zone_lookup"
-CLEAN_OUTPUT_PATH="/user/wl2484_nyu_edu/project/data/clean/tlc/zone_lookup"
-PROFILE_OUTPUT_PATH="/user/wl2484_nyu_edu/project/data/profile/tlc/zone_lookup"
+SOURCE_PATH="/user/wl2484_nyu_edu/project/data/source/tlc/zones"
+CLEAN_OUTPUT_PATH="/user/wl2484_nyu_edu/project/data/clean/tlc/zones"
+PROFILE_OUTPUT_PATH="/user/wl2484_nyu_edu/project/data/profile/tlc/zones"
 
 hadoop fs -rm -r ${CLEAN_OUTPUT_PATH}
 hadoop fs -rm -r ${PROFILE_OUTPUT_PATH}
