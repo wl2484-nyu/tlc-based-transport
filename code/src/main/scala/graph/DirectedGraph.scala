3f32b69f234a87ed9b9257e252f8abfc6e0969e2
narain-priyanka <62747395+narain-priyanka@users.noreply.github.com>
2023-12-09 02:32:37 +0530

    Update DirectedGraph.scala

--- code/src/main/scala/graph/DirectedGraph.scala
+++ code/src/main/scala/graph/DirectedGraph.scala
@@ -1,11 +1,11 @@
 package graph
 
 class DirectedGraph[Long](adjList: Map[Long, List[Long]]) extends Graph[Long] {
-  override def nodes: List[Long] = adjList.keys.toList
+  override def nodes: List[Long] = adjList.keys.toList //returning the list of keys from the (getconnectedlocationmap)
 
   override def edges: List[(Long, Long)] = adjList.flatMap {
     case (node, neighbours) => neighbours.map(neighbour => (node, neighbour))
-  }.toList
+  }.toList  // creating the list of nodes from the adjacency list (4,244) ,(4,232), (4,79)
 
   override def addNode(n: Long): Graph[Long] = new DirectedGraph(adjList + (n -> List()))
 
