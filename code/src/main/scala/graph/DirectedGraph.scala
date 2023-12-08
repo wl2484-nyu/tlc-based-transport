86f4e6e2f078f503509ec892c36547c9ad3169b8
narain-priyanka <62747395+narain-priyanka@users.noreply.github.com>
2023-12-09 01:25:23 +0530

    Update DirectedGraph.scala

--- code/src/main/scala/graph/DirectedGraph.scala
+++ code/src/main/scala/graph/DirectedGraph.scala
@@ -1,26 +1,19 @@
 package graph
 
-class DirectedGraph[N](adjList: Map[N, List[N]]) extends Graph[N] {
+class DirectedGraph[Long](adjList: Map[Long, List[Long]]) extends Graph[Long] {
+  override def nodes: List[Long] = adjList.keys.toList
 
-  override def nodes: List[N] = adjList.keys.toList
+  override def edges: List[(Long, Long)] = adjList.flatMap {
+    case (node, neighbours) => neighbours.map(neighbour => (node, neighbour))
+  }.toList
 
-  override def edges: List[(N, N)] = adjList.map {
-    case (node, neighbours) => {
-      neighbours.map(neighbour => (node, neighbour))
-    }
-  }.flatten.toList
+  override def addNode(n: Long): Graph[Long] = new DirectedGraph(adjList + (n -> List()))
 
-  override def addNode(n: N): Graph[N] = new DirectedGraph(adjList + (n -> List()))
-
-  override def addEdge(from: N, to: N): Graph[N] = {
+  override def addEdge(from: Long, to: Long): Graph[Long] = {
     val fromNeighbours = to +: neighbours(from)
     val g = new DirectedGraph(adjList + (from -> fromNeighbours))
-
     if (g.nodes.contains(to)) g else g.addNode(to)
-
   }
 
-  override def neighbours(node: N): List[N] = {
-    adjList.getOrElse(node, Nil)
-  }
+  override def neighbours(node: Long): List[Long] = adjList.getOrElse(node, Nil)
 }
