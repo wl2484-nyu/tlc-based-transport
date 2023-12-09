abfb0f1e52b2c258033de77ebca539fa3b06310b
Wan-Yu Lin <wl2484@nyu.edu>
2023-12-09 01:47:49 -0500

    Recover generics in graph classes

--- code/src/main/scala/graph/DirectedGraph.scala
+++ code/src/main/scala/graph/DirectedGraph.scala
@@ -1,19 +1,26 @@
 package graph
 
-class DirectedGraph[Long](adjList: Map[Long, List[Long]]) extends Graph[Long] {
-  override def nodes: List[Long] = adjList.keys.toList //returning the list of keys from the (getconnectedlocationmap)
+class DirectedGraph[N](adjList: Map[N, List[N]]) extends Graph[N] {
 
-  override def edges: List[(Long, Long)] = adjList.flatMap {
-    case (node, neighbours) => neighbours.map(neighbour => (node, neighbour))
-  }.toList  // creating the list of nodes from the adjacency list (4,244) ,(4,232), (4,79)
+  override def nodes: List[N] = adjList.keys.toList
 
-  override def addNode(n: Long): Graph[Long] = new DirectedGraph(adjList + (n -> List()))
+  override def edges: List[(N, N)] = adjList.map {
+    case (node, neighbours) => {
+      neighbours.map(neighbour => (node, neighbour))
+    }
+  }.flatten.toList
 
-  override def addEdge(from: Long, to: Long): Graph[Long] = {
+  override def addNode(n: N): Graph[N] = new DirectedGraph(adjList + (n -> List()))
+
+  override def addEdge(from: N, to: N): Graph[N] = {
     val fromNeighbours = to +: neighbours(from)
     val g = new DirectedGraph(adjList + (from -> fromNeighbours))
+
     if (g.nodes.contains(to)) g else g.addNode(to)
+
   }
 
-  override def neighbours(node: Long): List[Long] = adjList.getOrElse(node, Nil)
+  override def neighbours(node: N): List[N] = {
+    adjList.getOrElse(node, Nil)
+  }
 }
--- code/src/main/scala/graph/Graph.scala
+++ code/src/main/scala/graph/Graph.scala
@@ -1,13 +1,51 @@
 package graph
-trait Graph[Long] {
-  def nodes: List[Long]
-  def edges: List[(Long, Long)]
-  def addNode(n: Long): Graph[Long]
-  def addEdge(from: Long, to: Long): Graph[Long]
-  def neighbours(node: Long): List[Long]
+
+trait Graph[N] {
+
+  /**
+   * Function returns all nodes in the graph.
+   *
+   * @return list of nodes
+   */
+  def nodes: List[N]
+
+  /**
+   * Function returns all edges in the graph.
+   *
+   * @return list of tuples of nodes
+   */
+  def edges: List[(N, N)]
+
+
+  def addNode(n: N): Graph[N]
+
+  /**
+   * Function add an edge between start node and
+   * end node and returns a new graph which includes
+   * this edge.
+   *
+   * main.Graph is immutable.
+   *
+   * @param from node
+   * @param to   node
+   * @return new graph
+   */
+  def addEdge(from: N, to: N): Graph[N]
+
+  /**
+   * Function returns all neighbours of an input node.
+   *
+   * @param node
+   * @return list of nodes
+   */
+  def neighbours(node: N): List[N]
+
 }
 
 object Graph {
-  def apply[Long](adjList: Map[Long, List[Long]]): Graph[Long] = new DirectedGraph(adjList)
-  def apply[Long](): Graph[Long] = new DirectedGraph(Map[Long, List[Long]]())
+
+  def apply[N](adjList: Map[N, List[N]]): Graph[N] = new DirectedGraph(adjList)
+
+  def apply[N](): Graph[N] = new DirectedGraph(Map[N, List[N]]())
+
 }
