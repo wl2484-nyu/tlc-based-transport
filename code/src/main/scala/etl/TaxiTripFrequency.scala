9e2c306e08d0cee8cb7399ae6b812057baba4ee1
Wan-Yu Lin <wl2484@nyu.edu>
2023-12-09 01:55:33 -0500

    Reformat TaxiTripFrequency code lines and optimize imports

--- code/src/main/scala/etl/TaxiTripFrequency.scala
+++ code/src/main/scala/etl/TaxiTripFrequency.scala
@@ -1,15 +1,16 @@
 package etl
+
 import etl.Utils.{keyCleanOutput, keySource, loadintermediateData, parseOpts}
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
-import org.apache.spark.sql.types.{LongType, StructField, StructType}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{LongType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
 
 object TaxiTripFrequency {
   val spark = SparkSession.builder().appName("TaxiTripFrequency_ETL").getOrCreate()
   val schema = StructType(Array(StructField("pulocationID", LongType, true), StructField("dolocationID", LongType, true)))
 
-  def cleanRawData(rawDF: DataFrame,intermediateDF: DataFrame): DataFrame = {
+  def cleanRawData(rawDF: DataFrame, intermediateDF: DataFrame): DataFrame = {
     val isolatedLocations = List(103, 104, 105, 153, 194, 202)
     val manhattanLocationIDs = intermediateDF.select("location_id").distinct().collect().map(_.getInt(0))
 
@@ -29,30 +30,31 @@ object TaxiTripFrequency {
     frequencyDF.withColumn("PUDOPair", struct("pulocationID", "dolocationID")).select("PUDOPair", "count(1)").withColumnRenamed("count(1)", "Frequency")
 
   }
-  def saveCleanData(freqDF: DataFrame, cleanOutputPath: String,year: Int, cab: String): Unit = {
+
+  def saveCleanData(freqDF: DataFrame, cleanOutputPath: String, year: Int, cab: String): Unit = {
     freqDF.repartition(100).write.mode(SaveMode.Overwrite).parquet(s"$cleanOutputPath/${cab}/${year}/${cab}_intermediate_${year}.parquet")
   }
 
-//  def loadCleanSavedData(spark: SparkSession, path: String, cab: String, year:Int): DataFrame = {
-//    loadintermediateData(spark,path,year,cab)
+//  def loadCleanSavedData(spark: SparkSession, path: String, cab: String, year: Int): DataFrame = {
+//    loadintermediateData(spark, path, year, cab)
 //  }
 
   def main(args: Array[String]): Unit = {
     val years = Seq(2020, 2021, 2022, 2023)
-    val cabs = Seq("fhv","fhvhv","yellow","green")
+    val cabs = Seq("fhv", "fhvhv", "yellow", "green")
     val options = parseOpts(Map(), args.toList)
     val sourcePath = options(keySource).asInstanceOf[String]
     val cleanOutputPath = options(keyCleanOutput).asInstanceOf[String]
     val intermediatePath = "/user/wl2484_nyu_edu/project/data/intermediate/location_neighbors_distance/Manhattan/part-00000-7b78c933-8e72-43de-ad42-0ea0202969e4-c000.csv"
-    val intermediateDF= spark.read.option("header", true).option("inferSchema", true).csv(intermediatePath)
+    val intermediateDF = spark.read.option("header", true).option("inferSchema", true).csv(intermediatePath)
 
     var combinedRDD: RDD[Row] = spark.sparkContext.emptyRDD[Row]
 
     for (cab <- cabs; year <- years) {
-      val rawDF = loadintermediateData(spark, sourcePath,year, cab)
+      val rawDF = loadintermediateData(spark, sourcePath, year, cab)
       // cleaning
       val freqDF = cleanRawData(rawDF, intermediateDF)
-      saveCleanData(freqDF, cleanOutputPath,year,cab)
+      saveCleanData(freqDF, cleanOutputPath, year, cab)
     }
   }
 }
