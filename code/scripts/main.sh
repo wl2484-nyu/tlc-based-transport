cf423d642d3ec8002b3da889b8e37e26edce8b1a
Wan-Yu Lin <wl2484@nyu.edu>
2023-12-09 04:20:53 -0500

    Set app version to 1.0.0

--- code/scripts/main.sh
+++ code/scripts/main.sh
@@ -6,7 +6,7 @@
 #sbt package
 
 spark-submit --deploy-mode cluster \
-  --class Main tlc-based-transport_2.12-0.6.0.jar \
+  --class Main tlc-based-transport_2.12-1.0.0.jar \
   --ns-dis-input /user/wl2484_nyu_edu/project/data/intermediate/location_neighbors_distance \
   --tlc-input /user/wl2484_nyu_edu/project/data/clean/tlc/taxi_trips \
   --path-freq-output /user/wl2484_nyu_edu/project/data/intermediate/taxi_trip_path_frequency
