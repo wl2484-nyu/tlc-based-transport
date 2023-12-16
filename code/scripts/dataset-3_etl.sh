c1010b7a5826d639a03a422837a1ee09602accac
Wan-Yu Lin <wl2484@nyu.edu>
2023-12-15 22:39:09 -0500

    Align app versions in run scripts with build.sbt

--- code/scripts/dataset-3_etl.sh
+++ code/scripts/dataset-3_etl.sh
@@ -9,7 +9,7 @@ hadoop fs -rm -r /user/wl2484_nyu_edu/project/data/clean/tlc/zone_lookup
 hadoop fs -rm -r /user/wl2484_nyu_edu/project/data/profile/tlc/zone_lookup
 
 spark-submit --deploy-mode cluster \
-  --class etl.TaxiZoneLookup tlc-based-transport_2.12-0.6.0.jar \
+  --class etl.TaxiZoneLookup tlc-based-transport_2.12-1.1.1.jar \
   --source /user/wl2484_nyu_edu/project/data/source/tlc/zone_lookup \
   --clean-output /user/wl2484_nyu_edu/project/data/clean/tlc/zone_lookup \
   --profile-output /user/wl2484_nyu_edu/project/data/profile/tlc/zone_lookup
